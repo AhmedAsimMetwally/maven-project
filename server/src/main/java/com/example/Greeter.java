361104ee950d910dc17925111034c50fd6833ad7
AhmedAsimMetwally <ahmedassem.am@gmail.com>
2018-12-01 16:24:07 +0200

    fix comments

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,9 +13,9 @@ public class Greeter {
   }
 
 /**
-* @perm someone the name of a person
+* @param someone the name of a person
 * @return greeting string 
-*/  
+*/   
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
